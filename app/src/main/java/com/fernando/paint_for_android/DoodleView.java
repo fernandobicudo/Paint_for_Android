964c7431beb6dcbe61633d72c51a1a96509c4568
fernandobicudo <fernandobicudo@gmail.com>
2019-03-29 20:42:43 -0300

    Initial commit

---
+++ app/src/main/java/com/fernando/paint_for_android/DoodleView.java
@@ -0,0 +1,11 @@
+package com.fernando.paint_for_android;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DoodleView extends View {
+    public DoodleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
