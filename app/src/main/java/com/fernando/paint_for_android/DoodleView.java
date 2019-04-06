f9ee2f1103e61e9b54ea73093bf023840e81f054
fernandobicudo <fernandobicudo@gmail.com>
2019-04-05 22:15:09 -0300

    Métodos de drawinfg, até o touchStarted

--- app/src/main/java/com/fernando/paint_for_android/DoodleView.java
+++ app/src/main/java/com/fernando/paint_for_android/DoodleView.java
@@ -1,11 +1,137 @@
 package com.fernando.paint_for_android;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class DoodleView extends View {
+
+    private static final float TOUCH_TOLERANCE = 10;
+
+    private Bitmap bitmap;
+
+    private Canvas canvasBitmap;
+
+    private final Paint paintScreen;
+
+    private final Paint paintLine;
+
+    private final Map<Integer, Path> pathMap = new HashMap<>();
+
+    private final Map<Integer, Point> previousPointMap = new HashMap<>();
+
     public DoodleView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        paintScreen = new Paint();
+        paintLine = new Paint();
+
+        paintLine.setAntiAlias(true);
+
+        paintLine.setColor(Color.BLACK);
+
+        paintLine.setStrokeWidth(5);
+
+        paintLine.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+
+        canvasBitmap = new Canvas(bitmap);
+        bitmap.eraseColor(Color.WHITE);
+
+    }
+
+    public void clear() {
+        pathMap.clear();
+        previousPointMap.clear();
+        bitmap.eraseColor(Color.WHITE);
+        invalidate();
+
+    }
+
+    public void setDrawingColor(int color) {
+        this.paintLine.setColor(color);
+    }
+
+    public int getDrawingColor() {
+        return this.paintLine.getColor();
+    }
+
+    public void setLineWidth(int width) {
+        this.paintLine.setStrokeWidth(width);
+
+    }
+
+    public int getLineWidth() {
+        return (int) this.paintLine.getStrokeWidth();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, 0, 0, paintScreen);
+
+        Random random = new Random();
+
+        int a = random.nextInt(256);
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        paintLine.setColor(Color.argb(a, r, g, b));
+
+
+
+        for (Integer key : pathMap.keySet()) {
+            canvas.drawPath(pathMap.get(key), paintLine);
+        }
+
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getActionMasked();
+
+        int actionIndex = event.getActionIndex();
+
+        if (action == MotionEvent.ACTION_DOWN ||
+        action == MotionEvent.ACTION_POINTER_DOWN)
+        {
+
+        }
+        else if (action == MotionEvent.ACTION_UP ||
+        action == MotionEvent.ACTION_POINTER_UP){
+
+        }
+
+        else {
+
+        }
+        invalidate();
+
+
+        return true;
+    }
+
+    private void touchStarted(float x, float y, int lineID){
+        Path path;
+        Point point;
+
+        if (pathMap.containsKey(lineID)){
+            path = pathMap.get(lineID);
+        }
     }
 }
+
+
--- app/src/main/java/com/fernando/paint_for_android/MainActivityFragment.java
+++ app/src/main/java/com/fernando/paint_for_android/MainActivityFragment.java
@@ -1,5 +1,10 @@
 package com.fernando.paint_for_android;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,8 +14,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MainActivityFragment extends Fragment {
     private DoodleView doodleView;
@@ -46,11 +55,11 @@ public class MainActivityFragment extends Fragment {
             float z = event.values[2];
 
             lastAcceleration = currentAcceleration;
-            currentAcceleration = x * x + y * y + z *z;
+            currentAcceleration = x * x + y * y + z * z;
             acceleration = currentAcceleration * (currentAcceleration - lastAcceleration);
 
             if (acceleration > ACCELERATION_THRESHOLD) {
-                //confirmErase();
+                confirmErase();
             }
 
         }
@@ -61,5 +70,118 @@ public class MainActivityFragment extends Fragment {
         }
     };
 
+    private void confirmErase() {
+        EraseImageDialogFragment eraseImageDialogFragment = new EraseImageDialogFragment();
+
+        eraseImageDialogFragment.show(getFragmentManager(), "erase dialog");
+    }
+
+    private void enableAccelerometerListening() {
+        SensorManager sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+
+        sensorManager.registerListener(listener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void disableAccelerometer() {
+        SensorManager sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.unregisterListener(listener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableAccelerometer();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        enableAccelerometerListening();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.doodle_fragment_menu, menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.color:
+                ColorDialogFragment colorDialogFragment = new ColorDialogFragment();
+                colorDialogFragment.show(getFragmentManager(), "color dialog");
+                return true;
+            case R.id.line_width:
+                LineWidthDialogFragment lineWidthDialogFragment = new LineWidthDialogFragment();
+                lineWidthDialogFragment.show(getFragmentManager(), "line width dialog");
+                return true;
+            case R.id.print:
+                //doodleView.printImage();
+                return true;
+            case R.id.delete_drawing:
+                confirmErase();
+                return true;
+            case R.id.save:
+                //saveImage();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void saveImage() {
+        if (getContext().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage(R.string.permission_explanation);
+                builder.setPositiveButton(android.R.string.ok, (d, w) -> {
+                    requestPermissions(
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            SAVE_IMAGE_PERMISSION_REQUEST_CODE);
+                });
+                builder.create().show();
+            } else {
+                requestPermissions(new String[]{
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        SAVE_IMAGE_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            //doodleView.saveImage();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull
+            String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case SAVE_IMAGE_PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    //doodleView.saveImage();//faremos já já
+                }
+                else {
+                    Toast.makeText(getActivity(), getString(R.string.permission_explanation),
+                            Toast.LENGTH_SHORT).show();
+
+                }
+        }
+    }
+
+    public DoodleView getDoodleView(){
+        return this.doodleView;
+    }
+    public void setDialogOnScreen(boolean dialogOnScreen) {
+        this.dialogOnScreen = dialogOnScreen;
+    }
+
 }
+
+
+
+
 
